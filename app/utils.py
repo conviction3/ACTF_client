af1c517e8ac1f8858b77a9b34d31fb0759a7af45
Bian <374044291@qq.com>
2020-12-17 03:42:53 +0800

    + 生成数据的脚本 + 测试求和

--- app/utils.py
+++ app/utils.py
@@ -3,10 +3,12 @@ import csv
 
 def read_csv_int(file_name: str, from_index: int, to_index: int) -> list:
     """
-    todo: 分块读取大文件
     read data from csv file
-    :param file_name:
+    :param file_name:   要读取的文件名
+    :param from_index: 开始下标，从0开始，包括from_index
+    :param to_index: 结束下标，从0开始，不包括to_index
     :return: list of int
+    todo: 分块读取大文件
     """
     result_list = []
     with open(file_name, 'r') as f:
@@ -15,3 +17,13 @@ def read_csv_int(file_name: str, from_index: int, to_index: int) -> list:
         for item in reader:
             result_list.append(item[0])
     return [int(t) for t in result_list[from_index:to_index]]
+
+
+def add(data_list: list) -> int:
+    """
+    对list of int 求和
+    :param data_list: list of int
+    :return: sum
+    todo: 拓展类型
+    """
+    return sum(data_list)
--- test/test_utils.py
+++ test/test_utils.py
@@ -9,3 +9,8 @@ class UtilsTestSuite(unittest.TestCase):
         # data = read_csv_int(file_name=test_file_name, from_index=0, to_index=5)
         data = read_csv_int(file_name=test_file_name, from_index=0, to_index=4)
         self.assertEqual([1, 2, 3, 4], data)
+
+    def test_add(self):
+        data = read_csv_int(file_name=test_file_name, from_index=0, to_index=4)
+        sum = add(data)
+        self.assertEqual(10, sum)
